74394527fa773f0a3990ea6216432a0602f3223c
Teddy Conyers Jr <54686195+tc3941@users.noreply.github.com>
2020-02-20 20:14:37 -0500

    Adding all files

---
+++ Spell Book/Fire_Bomb_Spell_Book.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_Bomb_Spell_Book : Spell_Book
+{
+    
+}
---
+++ Spell Book/Spell_Book.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spell_Book : Items
+{
+    
+    public Spell spell;
+    public GameObject spell_orb;
+    private void Start()
+    {
+        spellBook = true;
+    }
+
+    public override Spell GetSpell()
+    {
+        return spell;
+    }
+
+    public override Spell_Book GetSpellBook()
+    {
+        return this;
+    }
+
+}
